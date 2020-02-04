5d0d8051cab8e827ef954915b5cfec30917bc2c8
Chris Aitken <chris@heythisischris.com>
2020-02-04 00:41:33 -0500

    Created empty Svelte project using svelte-routing for navigation

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { Router, Route } from "svelte-routing";
+  import NavLink from "./components/NavLink.svelte";
+  import Home from "./routes/Home.svelte";
+  import Test from "./routes/Test.svelte";
+
+  // Used for SSR. A falsy value is ignored by the Router.
+  export let url = "";
+</script>
+
+<Router url="{url}">
+  <nav>
+    <NavLink to="/">Home</NavLink>
+    <NavLink to="resume">Resume</NavLink>
+    <NavLink to="apps">Apps</NavLink>
+    <NavLink to="sites">Sites</NavLink>
+    <NavLink to="film">Film</NavLink>
+    <NavLink to="music">Music</NavLink>
+  </nav>
+  <div>
+    <Route path="/" component="{Home}" />
+    <Route path="resume" component="{Test}" />
+  </div>
+</Router>
---
+++ src/routes/Test.svelte
@@ -0,0 +1,2 @@
+<h1>Resume</h1>
+<p>This is a test route</p>
