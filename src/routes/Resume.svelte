9a74e266e27c759089a74d524745d205ee3c6298
Chris Aitken <chris@heythisischris.com>
2020-07-14 22:31:23 -0400

    Minor updates to resume section

--- src/routes/Resume.svelte
+++ src/routes/Resume.svelte
@@ -1,6 +1,6 @@
 <h1>Objective</h1>
 <div class="textBlock">
-  <p>To develop robust software in an agile environment</p>
+  <p>To develop novel and impactful cross-platform software</p>
 </div>
 <h1>Education</h1>
 <div class="textBlock">
@@ -22,15 +22,16 @@
   </p>
   <ul style="margin-top:-15px;">
     <li>
-      Began as an intern during Summer 2016 finding and fixing bugs in iOS and
-      Android apps
+      Began as an intern during Summer 2016 finding and fixing bugs in iOS/Android apps
     </li>
     <li>
-      Accepted a paid position as a programmer afterwards and worked on these
-      apps and web services:
+      Accepted a paid position afterwards and worked on these projects:
       <ul>
         <li>
-          <a target="_blank" href="https://corkagefee.com">CorkageFee</a> &ndash; View wine bottle fees at nearby restaurants
+          <a target="_blank" href="https://apps.apple.com/us/app/teckpert-digital-tech-work/id1474005004">TECKpert App</a> &ndash; Freelancing app for tech professionals (built mobile app/backend)
+        </li>
+        <li>
+          <a target="_blank" href="https://corkagefee.com">CorkageFee</a> &ndash; View wine bottle fees at nearby restaurants (maintaining mobile app/backend)
         </li>
         <li>
            <a target="_blank" href="https://realdash.com">RealDash</a> &ndash; Platform for real estate professionals
@@ -54,15 +55,15 @@
       Created/maintained WordPress plugins for various client websites in PHP
     </li>
     <li>
-      Experience with Serverless Applications on AWS for <a target="_blank" href="https://hospitalistapp.com">Hospitalist Assist</a>, a patient
-      admitting portal
+      Developed serverless apps on AWS for <a target="_blank" href="https://hospitalistapp.com">Hospitalist Assist</a>, a patient
+      admitting portal, and the <a target="_blank" href="https://apps.apple.com/us/app/teckpert-digital-tech-work/id1474005004">TECKpert App</a>, a freelancing platform for tech professionals
       <ul>
         <li>
           Developed multiple client backends using Node.js + Lambda, frontend
           using React Native
         </li>
         <li>
-          Utilize Amazon Cognito for user authentication, Amazon Lambda for
+          Utilized Amazon Cognito for user authentication, Amazon Lambda for
           serverless API calls, API Gateway to route calls to Lambda, and Amazon
           RDS to host a MySQL instance or DynamoDB to host a NoSQL instance
           <br />
@@ -75,11 +76,10 @@
   <br>Lexus Eco Challenge, Miami, Florida</p>
   <ul style="margin-top:-15px">
     <li>
-      Developed <a target="_blank" href="https://dontbeadrain.com">dontbeadrain.com</a> which highlighted water conservation tips using HTML5
-      and CSS3.
+      Developed <a target="_blank" href="https://dontbeadrain.com">dontbeadrain.com</a> which highlighted water conservation tips
     </li>
     <li>
-      Won 1st Place in National Competition, received over $20,000 in funding for our school
+      Won 1st Place in National Competition, received $20,000 in funding for our science department
     </li>
   </ul>
   <p>
@@ -91,7 +91,7 @@
       Programmed several Flash games
     </li>
     <li>
-      Sold <a target="_blank" href="https://www.kongregate.com/games/fettspielen/dodge">Dodge</a> to Fettspielen.de, a German game portal.
+      Sold <a target="_blank" href="https://www.kongregate.com/games/fettspielen/dodge">Dodge</a> to Fettspielen.de, a German game portal
     </li>
   </ul>
 </div>
@@ -124,6 +124,6 @@
     <strong>IDEs:</strong>
     VS Code, Xcode, Android Studio<br>
     <strong>Idiomas:</strong>
-    Español
+    Inglés y Español
   </p>
 </div>
